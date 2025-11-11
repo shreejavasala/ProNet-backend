6b03b80099d9149545d290b0e35dc089ea470c36
shreejavasala <shreevasala29@gmail.com>
2025-11-11 23:17:58 +0530

    Update allowedOrigins.js

--- configs/allowedOrigins.js
+++ configs/allowedOrigins.js
@@ -1,7 +1,6 @@
 const allowedOrigins = [
   'http://localhost:5173',
   'https://pro-net-frontend.vercel.app',
-  'https://pro-net-frontend.vercel.app/login'
-]
+];
 
 export default allowedOrigins;
