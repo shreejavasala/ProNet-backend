d1c6325e0deb6801b187f74afcc91e2ff27930bf
shreejavasala <shreevasala29@gmail.com>
2025-11-11 21:52:59 +0530

    Update CORS

--- configs/allowedOrigins.js
+++ configs/allowedOrigins.js
@@ -1,5 +1,7 @@
 const allowedOrigins = [
-  'http://localhost:5173'
+  'http://localhost:5173',
+  'https://pro-net-frontend.vercel.app',
+  'https://pro-net-frontend.vercel.app/login'
 ]
 
 export default allowedOrigins;
